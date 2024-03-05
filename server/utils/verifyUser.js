640391ef82d03ff19fbc824681945a30d82e8a31
unknown <smartkelvin66@gmail.com>
2024-03-05 15:31:34 +0100

    complete-update-functionality

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -4,7 +4,8 @@ import { errorHandler } from "../utils/error.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
-  if (!token) return next(errorHandler(401, "You are not authorized!"));
+  if (!token)
+    return next(errorHandler(401, "You are not authorized!, please sign in"));
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, "Token is not valid!"));
     req.user = user;
