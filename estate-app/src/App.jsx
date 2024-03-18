e008cb3e4af1172c2e68c768e5ed39fa7904f5ec
unknown <smartkelvin66@gmail.com>
2024-03-18 15:35:14 +0100

    update-list-functionality

--- estate-app/src/App.jsx
+++ estate-app/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp";
 import Header from "./component/Header";
 import PrivateRoute from "./component/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 const App = () => {
   return (
@@ -21,6 +22,10 @@ const App = () => {
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
